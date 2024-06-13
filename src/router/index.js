a7cd225958d2b9445242e3cdad1f2dc1ec745a4a
Jacob <lewandowskiJakub9@gmail.com>
2024-06-12 20:29:02 -0400

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import StartGame from '@/views/StartGame.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {path: '/', name: 'home', component: StartGame}
+  ]
+})
+
+export default router
---
+++ src/views/StartGame.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Hello, this is a test of the font that was selected to be used for this Gwent MMO project</h1>
+    <div class="">
+        
+    </div>
+</template>
+
+<style scoped>
+</style>
