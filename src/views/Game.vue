d59d0680a62eec373e4e0f53df99416176f13481
Jacob <lewandowskiJakub9@gmail.com>
2024-08-10 23:28:15 -0400

    This commit finishes the walk animation for the player character

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,21 +1,43 @@
 <script>
 import map_imgSrc from "@/assets/images/gwent_tavern_map.png";
 import map_foreground_imgSrc from "@/assets/images/tavern_map_foreground.png";
+import player_upImgSrc from "@/assets/images/playerUp.png";
 import player_downImgSrc from "@/assets/images/playerDown.png";
+import player_leftImgSrc from "@/assets/images/playerLeft.png";
+import player_rightImgSrc from "@/assets/images/playerRight.png";
 
 export default {
   data() {
     return {
       map_imgSrc,
       map_foreground_imgSrc,
+      player_upImgSrc,
       player_downImgSrc,
-      
+      player_leftImgSrc,
+      player_rightImgSrc,
+      isDaytime: true,
     };
   },
 
-  methods() {},
+  computed: {
+    gameTimeClass() {
+      return this.isDaytime ? "game__time-day" : "game__time-night";
+    },
+  },
+
+  methods: {
+    dayNightCycle() {
+      const date = new Date();
+      let timeOfDay = date.getHours();
+
+      this.isDaytime = timeOfDay >= 7 && timeOfDay <= 19;
+    },
+  },
 
   mounted() {
+    // Check what time is at users side and change ingame time accordingly
+    this.dayNightCycle();
+
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
 
@@ -23,37 +45,45 @@ export default {
     canvas.height = 720;
 
     const offset = {
-      x: -2500,
-      y: -1900,
+      x: -2525,
+      y: -1880,
     };
 
     //Return the image based on img.src
     function getImage(imgSrc) {
-        const image = new Image();
-        image.src = imgSrc;
-        return image;
+      const image = new Image();
+      image.src = imgSrc;
+      return image;
     }
 
     const mapImage = getImage(map_imgSrc);
+
     const mapForegroundImage = getImage(map_foreground_imgSrc);
-    const playerImage = getImage(player_downImgSrc);
+
+    const playerSprites = {
+      up: getImage(player_upImgSrc),
+      down: getImage(player_downImgSrc),
+      left: getImage(player_leftImgSrc),
+      right: getImage(player_rightImgSrc),
+    };
 
     //Create new Sprite Class
     class Sprite {
-      constructor({ image, position, frames = { max: 1 } }) {
+      constructor({ image, position, frames = { max: 1 }}) {
         this.image = image;
         this.position = position;
-        this.frames = frames;
+        this.frames = { ...frames, val: 0, elapsed: 0 };
         this.image.onload = () => {
           this.width = this.image.width / this.frames.max;
           this.height = this.image.height;
         };
+        this.moving = false;
       }
 
       draw() {
         ctx.drawImage(
           this.image,
-          0,
+          this.frames.val * this.width,
           0,
           this.image.width / this.frames.max,
           this.image.height,
@@ -62,10 +92,24 @@ export default {
           this.image.width / this.frames.max,
           this.image.height
         );
+
+        if (this.moving) {
+          if (this.frames.max > 1) {
+            this.frames.elapsed++;
+          }
+
+          if (this.frames.elapsed % 10 == 0) {
+            if (this.frames.val < this.frames.max - 1) {
+              this.frames.val++;
+            } else {
+              this.frames.val = 0;
+            }
+          }
+        }
       }
     }
 
-    //Create Map & player Sprites
+    //Create Map & player playerSprites
     const map = new Sprite({
       image: mapImage,
       position: {
@@ -82,13 +126,20 @@ export default {
       },
     });
 
+    const playerImage = playerSprites.down;
     const player = new Sprite({
       image: playerImage,
       position: {
-        x: canvas.width / 2 + 264 / 8,
-        y: canvas.height / 2 - 79 / 2,
+        x: canvas.width / 2 - playerImage.width / 8,
+        y: canvas.height / 2 - playerImage.height / 2,
       },
       frames: { max: 4 },
+      playerSprites: {
+        up: playerSprites.up,
+        down: playerSprites.down,
+        left: playerSprites.left,
+        right: playerSprites.right,
+      },
     });
 
     let keys = {
@@ -100,7 +151,7 @@ export default {
 
     let lastKey = "";
     window.addEventListener("keydown", (e) => {
-      switch (e.key.toLowerCase()) {
+      switch (e.key) {
         case "w":
           lastKey = "w";
           keys.w.pressed = true;
@@ -124,64 +175,79 @@ export default {
     });
 
     window.addEventListener("keyup", (e) => {
-      switch (e.key.toLowerCase()) {
+      switch (e.key) {
         case "w":
           keys.w.pressed = false;
+          player.moving = false;
+          player.frames.val = 0;
           break;
 
         case "s":
           keys.s.pressed = false;
+          player.moving = false;
+          player.frames.val = 0;
           break;
 
         case "a":
           keys.a.pressed = false;
+          player.moving = false;
+          player.frames.val = 0;
           break;
 
         case "d":
           keys.d.pressed = false;
+          player.moving = false;
+          player.frames.val = 0;
           break;
       }
     });
 
     function move() {
       if (keys.w.pressed && lastKey == "w") {
+        player.image = playerSprites.up;
         player.position.y -= 4;
-        canvas.translate
+        ctx.translate(0, 4);
+        player.moving = true;
       }
+
       if (keys.s.pressed && lastKey == "s") {
+        player.image = playerSprites.down;
         player.position.y += 4;
+        ctx.translate(0, -4);
+        player.moving = true;
       }
+
       if (keys.a.pressed && lastKey == "a") {
+        player.image = playerSprites.left;
         player.position.x -= 4;
+        ctx.translate(4, 0);
+        player.moving = true;
       }
+
       if (keys.d.pressed && lastKey == "d") {
+        player.image = playerSprites.right;
         player.position.x += 4;
+        ctx.translate(-4, 0);
+        player.moving = true;
       }
     }
 
-  
-
+    //For testingpurposes only (remove once everything works as should)
     function positions() {
+      console.log(
+        "Player_1 Position: X:" + player.position.x + ", Y:" + player.position.y
+      );
 
-        console.log(
-          "Player Position: X:" +
-            player.position.x +
-            ", Y:" +
-            player.position.y
-        );
-        console.log(
-          "Canvas Position: X:" +
-            map.position.x +
-            ", Y:" +
-            map.position.y
-        );
+      console.log(
+        "Canvas Position: X:" + map.position.x + ", Y:" + map.position.y
+      );
     }
 
     window.addEventListener("keydown", (e) => {
-        if (e.key == "Enter") {
-            positions();
-        }});
-
+      if (e.key == "Enter") {
+        positions();
+      }
+    });
 
     //Game Loop
     function game() {
@@ -192,9 +258,6 @@ export default {
 
       // Move player
       move();
-
-
-
     }
     //Initiate the Game once
     let runOnce = false;
@@ -206,7 +269,46 @@ export default {
 </script>
 
 <template>
-  <canvas class="canvas"></canvas>
+  <div class="game">
+    <canvas></canvas>
+    <div :class="['game__time', gameTimeClass]"></div>
+  </div>
 </template>
 
-<style></style>
+<style>
+.game {
+  width: 1280px;
+  height: 720px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.game__time-night {
+  width: inherit;
+  height: inherit;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(12, 20, 124, 0.5);
+  z-index: 2;
+}
+
+.game__time-day {
+  width: inherit;
+  height: inherit;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(12, 20, 124, 0);
+  z-index: 2;
+}
+
+canvas {
+  position: absolute;
+  z-index: 1;
+}
+</style>
